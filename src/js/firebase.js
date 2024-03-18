91c534f6c5e1cc16da5e9cfad7f222c55e10d934
kane <kanet0916@gmail.com>
2024-03-18 19:06:44 +0900

    signup_finish

--- src/js/firebase.js
+++ src/js/firebase.js
@@ -1,11 +1,15 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+//import { initializeApp } from "firebase/app";
+//import { getAnalytics } from "firebase/analytics";
+//import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
+import { initializeApp } from 'https://www.gstatic.com/firebasejs/10.4.0/firebase-app.js'
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// If you enabled Analytics in your project, add the Firebase SDK for Google Analytics
+import { getAnalytics } from 'https://www.gstatic.com/firebasejs/10.4.0/firebase-analytics.js'
+
+// Add Firebase products that you want to use
+import { getAuth, createUserWithEmailAndPassword} from 'https://www.gstatic.com/firebasejs/10.4.0/firebase-auth.js'
+import { getFirestore } from 'https://www.gstatic.com/firebasejs/10.4.0/firebase-firestore.js'
 const firebaseConfig = {
   apiKey: "AIzaSyA7owjrk31aiD5CY_JK_AWUSMcJU3NDJWs",
   authDomain: "karatoapp.firebaseapp.com",
@@ -19,3 +23,24 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
+// Initialize Firebase Authentication and get a reference to the service
+const auth = getAuth();
+let email = "example@gmail.com";
+let password ="12345";
+window.signup = ()=>{
+    email = document.getElementById("emailInput");
+    password = document.getElementById("passwordInput");
+    console.log("testfunc");
+    createUserWithEmailAndPassword(auth, email.value, password.value)
+    .then((userCredential) => {
+        // Signed in
+        const user = userCredential.user;
+        // ...
+    })
+    .catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        console.log(error.message);
+        // ..
+    });
+};
