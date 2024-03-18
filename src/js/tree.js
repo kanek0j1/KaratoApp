99808afd4a7218225261a3e7eec1220437ff8086
sato3100 <satoru4107@gmail.com>
2024-03-18 12:50:39 +0900

    tree.js修正

--- src/js/tree.js
+++ src/js/tree.js
@@ -1,14 +1,13 @@
 function setup() {
-    let cnv = createCanvas(windowWidth * 0.85, windowHeight / 1.11); // キャンバスを作成
+    let cnv = createCanvas(windowWidth * 0.85, windowHeight * 0.9); // キャンバスを作成
     cnv.parent('canvas-container'); // キャンバスをmainタグ内に配置している
     noLoop(); // draw関数を1回だけ実行
 }
 
 function draw() {
-    background(235); // キャンバスの背景色を指定
     // ここにtreeのコードを追加していく！！！！
 }
 
 function windowResized() {
-    resizeCanvas(windowWidth * 0.85, windowHeight); // ウィンドウサイズが変更された際にキャンバスサイズを再調整
+    resizeCanvas(windowWidth * 0.85, windowHeight * 0.9); // ウィンドウサイズが変更された際にキャンバスサイズを再調整
 }
