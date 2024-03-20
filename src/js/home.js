25f22ffda010951ba381463878b0fdade198fbe1
Tatsuya Fukui <t.fukui@prog-8.com>
2024-03-20 10:37:31 +0900

    fix

--- src/js/home.js
+++ src/js/home.js
@@ -1,50 +1,56 @@
-import { initializeApp } from 'https://www.gstatic.com/firebasejs/10.4.0/firebase-app.js'
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.4.0/firebase-app.js";
 
 // If you enabled Analytics in your project, add the Firebase SDK for Google Analytics
-import { getAnalytics } from 'https://www.gstatic.com/firebasejs/10.4.0/firebase-analytics.js'
+import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.4.0/firebase-analytics.js";
 
 // Add Firebase products that you want to use
-import { getAuth, onAuthStateChanged } from 'https://www.gstatic.com/firebasejs/10.4.0/firebase-auth.js'
-import { getFirestore, doc, setDoc, getDoc } from 'https://www.gstatic.com/firebasejs/10.4.0/firebase-firestore.js'
+import {
+	getAuth,
+	onAuthStateChanged,
+} from "https://www.gstatic.com/firebasejs/10.4.0/firebase-auth.js";
+import {
+	getFirestore,
+	doc,
+	setDoc,
+	getDoc,
+} from "https://www.gstatic.com/firebasejs/10.4.0/firebase-firestore.js";
 const firebaseConfig = {
-  apiKey: "AIzaSyA7owjrk31aiD5CY_JK_AWUSMcJU3NDJWs",
-  authDomain: "karatoapp.firebaseapp.com",
-  projectId: "karatoapp",
-  storageBucket: "karatoapp.appspot.com",
-  messagingSenderId: "447206705595",
-  appId: "1:447206705595:web:5bd3cc4aaf74c1260d9c9e",
-  measurementId: "G-FD8ER0LNDR"
+	apiKey: "AIzaSyA7owjrk31aiD5CY_JK_AWUSMcJU3NDJWs",
+	authDomain: "karatoapp.firebaseapp.com",
+	projectId: "karatoapp",
+	storageBucket: "karatoapp.appspot.com",
+	messagingSenderId: "447206705595",
+	appId: "1:447206705595:web:5bd3cc4aaf74c1260d9c9e",
+	measurementId: "G-FD8ER0LNDR",
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
 
+document.addEventListener("DOMContentLoaded", () => {
+	const auth = getAuth();
+	onAuthStateChanged(auth, async (user) => {
+		if (user) {
+			const uid = user.uid;
+			const db = getFirestore();
+			const userDocRef = doc(db, "users", uid);
+			const treeDocRef = doc(db, "tree", uid);
+			const userDoc = await getDoc(userDocRef);
+			const treeDoc = await getDoc(treeDocRef);
 
-document.addEventListener('DOMContentLoaded', () => {
-    const auth = getAuth();
-    onAuthStateChanged(auth, async(user) => {
-      if (user) {
-        const uid = user.uid;
-        const db = getFirestore();
-        const userDocRef = doc(db, "users", uid);
-        const treeDocRef = doc(db, "tree", uid);
-        const userDoc = await getDoc(userDocRef);
-        const treeDoc = await getDoc(treeDocRef);
+			if (userDoc.exists()) {
+				document.querySelector(".name").innerText = userDoc.data().name;
+				document.querySelector(".days").innerText =
+					treeDoc.data().level + "days";
 
-
-        if (userDoc.exists()) {
-          document.querySelector('.name').innerText = userDoc.data().name;
-          document.querySelector('.days').innerText = treeDoc.data().level + "days";
-
-          localStorage.setItem('level', treeDoc.data().level);
-          localStorage.setItem('name', userDoc.data().name);
-
-        } else {
-          console.log("User document does not exist.");
-        }
-      } else {
-        console.log("No user is signed in.");
-      }
-    });
-  });
+				localStorage.setItem("level", treeDoc.data().level);
+				localStorage.setItem("name", userDoc.data().name);
+			} else {
+				console.log("User document does not exist.");
+			}
+		} else {
+			console.log("No user is signed in.");
+		}
+	});
+});
--- src/js/tree.js
+++ src/js/tree.js
@@ -1,168 +1,174 @@
-
-
 let growth = 0.0;
 let leafColor = [139, 209, 89];
 
-
-
 function setup() {
-    let cnv = createCanvas(windowWidth * 0.85, windowHeight * 0.85);
-    cnv.parent('canvas-container');
+	let cnv = createCanvas(windowWidth * 0.85, windowHeight * 0.85);
+	cnv.parent("canvas-container");
 }
 
 function windowResized() {
-    resizeCanvas(windowWidth * 0.85, windowHeight * 0.85);
+	resizeCanvas(windowWidth * 0.85, windowHeight * 0.85);
 }
 
 function draw() {
-    background(255);
-    // console.log("start");
+	background(255);
+	// console.log("start");
 
-    let commands = genCommand(growth);
-    let distance = 1 * growth;
-    let angle = radians(25);
+	let commands = genCommand(growth);
+	let distance = 1 * growth;
+	let angle = radians(25);
 
-    translate(width / 2, height);
-    let depth = 0;
+	translate(width / 2, height);
+	let depth = 0;
 
-    for (let i = 0; i < commands.length; i++) {
-        let currentCmd = commands.charAt(i);
-        switch (currentCmd) {
-            case "F":
-                stroke("#C27526")
-                strokeWeight(map(depth, 0, growth, 5, 0));
-                line(0, 0, 0, -distance);
-                translate(0, -distance);
-                break;
-            case "+":
-                rotate(angle);
-                    drawLeaf();
-                break;
-            case "-":
-                rotate(-angle);
-                    drawLeaf();
-                break;
-            case "[":
-                push();
-                depth++;
-                break;
-            case "]":
-                pop();
-                depth--;
-                break;
-        }
-    }
-    // console.log("end");
-    let level = localStorage.getItem('level');
-console.log(level);
+	for (let i = 0; i < commands.length; i++) {
+		let currentCmd = commands.charAt(i);
+		switch (currentCmd) {
+			case "F":
+				stroke("#C27526");
+				strokeWeight(map(depth, 0, growth, 5, 0));
+				line(0, 0, 0, -distance);
+				translate(0, -distance);
+				break;
+			case "+":
+				rotate(angle);
+				drawLeaf();
+				break;
+			case "-":
+				rotate(-angle);
+				drawLeaf();
+				break;
+			case "[":
+				push();
+				depth++;
+				break;
+			case "]":
+				pop();
+				depth--;
+				break;
+		}
+	}
+	// console.log("end");
+	let level = localStorage.getItem("level");
+	console.log(level);
 
-growth = frameCount * 0.05;
-if (1 == level) {
-    growth = 0.15;
-} else {
-    growth = level * 0.3;
-}
+	growth = frameCount * 0.05;
+	if (1 == level) {
+		growth = 0.15;
+	} else {
+		growth = level * 0.3;
+	}
 
-// 画像をBase64形式の文字列に変換
-let base64String = png.canvas.toDataURL();
+	// 画像をBase64形式の文字列に変換
+	// let base64String = png.canvas.toDataURL();
 
-// ローカルストレージに保存
-localStorage.setItem('image_' + level, base64String);
+	// ローカルストレージに保存
+	// localStorage.setItem('image_' + level, base64String);
 
+	// canvasを画像に変換
+	// let canvas = document.getElementById('defaultCanvas0');
+	// console.log(canvas);
 
+	document.addEventListener("DOMContentLoaded", () => {
+		// URLSearchParamsを使用してクエリパラメータを取得
+		const urlParams = new URLSearchParams(window.location.search);
+		const happyValue = urlParams.get("happy");
+		const angryValue = urlParams.get("angry");
+		const sadValue = urlParams.get("sad");
+		const funValue = urlParams.get("fun");
+		console.log("happy = " + happyValue);
+		console.log("angry = " + angryValue);
+		console.log("sad = " + sadValue);
+		console.log("fun = " + funValue);
 
+		// 値の変換と葉の色の変更
+		const happyclore = parseInt(happyValue);
+		const angryclore = parseInt(angryValue);
+		const sadclore = parseInt(sadValue);
+		const funclore = parseInt(funValue);
 
-document.addEventListener('DOMContentLoaded', () => {
-    // URLSearchParamsを使用してクエリパラメータを取得
-    const urlParams = new URLSearchParams(window.location.search);
-    const happyValue = urlParams.get('happy');
-    const angryValue = urlParams.get('angry');
-    const sadValue = urlParams.get('sad');
-    const funValue = urlParams.get('fun');
-    console.log("happy = " + happyValue);
-    console.log("angry = " + angryValue);
-    console.log("sad = " + sadValue);
-    console.log("fun = " + funValue);
+		// クエリパラメータを含むURLに遷移
+		//window.location.href = '../home.html?' +  parseInt.toInt();
 
+		// 葉の色を変更する（この関数の実装は省略）
+		changeLeafColor(happyclore, angryclore, sadclore, funclore);
+	});
 
+	function changeLeafColor(happyclore, angryclore, sadclore, funclore) {
+		let r = 0,
+			g = 0,
+			b = 0;
 
-        // 値の変換と葉の色の変更
-        const happyclore = parseInt(happyValue);
-        const angryclore = parseInt(angryValue);
-        const sadclore = parseInt(sadValue);
-        const funclore = parseInt(funValue);
+		// 数値に変換
+		let happyValue = parseInt(happyclore);
+		let angryValue = parseInt(angryclore);
+		let sadValue = parseInt(sadclore);
+		let funValue = parseInt(funclore);
 
-// クエリパラメータを含むURLに遷移
-//window.location.href = '../home.html?' +  parseInt.toInt();
+		// 値に基づいて色を変更
+		if (happyValue + angryValue + sadValue + funValue <= 4) {
+			(r = 139), (g = 209), (b = 89);
+		} else {
+			if (happyValue) {
+				// 喜び（黄色を強調）
+				r += 40 * happyValue;
+				g += 40 * happyValue;
+				b += 2 * happyValue;
+			}
+			if (angryValue) {
+				// 怒り（赤を強調）
+				r += 50 * angryValue;
+				g -= 30 * angryValue;
+				b -= 30 * angryValue;
+			}
+			if (sadValue) {
+				// 哀しみ（青を強調）
+				r -= 10 * sadValue;
+				g -= 10 * sadValue;
+				b += 110 * sadValue;
+			}
+			if (funValue) {
+				// 楽しい（緑を強調し全体的に明るく）
+				r -= 5 * funValue;
+				g += 50 * funValue;
+				b -= 5 * funValue;
+			}
+		}
+		leafColor = [r, g, b];
+	}
 
- // 葉の色を変更する（この関数の実装は省略）
-changeLeafColor(happyclore, angryclore, sadclore, funclore);
-});
+	function drawLeaf() {
+		push();
+		fill(...leafColor);
+		noStroke();
+		ellipse(0, 0, 13, 6);
+		pop();
+	}
 
-function changeLeafColor(happyclore, angryclore, sadclore, funclore) {
-    let r = 0, g = 0, b = 0;
-
-    // 数値に変換
-    let happyValue = parseInt(happyclore);
-    let angryValue = parseInt(angryclore);
-    let sadValue = parseInt(sadclore);
-    let funValue = parseInt(funclore);
-
-// 値に基づいて色を変更
-if(happyValue + angryValue + sadValue + funValue <= 4){
- r = 139, g = 209, b = 89;
-}else {
-    if (happyValue) { // 喜び（黄色を強調）
-        r += 40 * happyValue; g += 40 * happyValue; b += 2 * happyValue;
-    }
-    if (angryValue) { // 怒り（赤を強調）
-        r += 50 * angryValue; g -= 30 * angryValue; b -= 30 * angryValue;
-    }
-    if (sadValue) { // 哀しみ（青を強調）
-        r -= 10 * sadValue; g -= 10 * sadValue; b += 110 * sadValue;
-    }
-    if (funValue) { // 楽しい（緑を強調し全体的に明るく）
-        r -= 5 * funValue; g += 50 * funValue; b -= 5 * funValue;
-    }
-}
-    leafColor = [r, g, b];
-
-}
-
-function drawLeaf() {
-    push();
-    fill(...leafColor);
-    noStroke();
-    ellipse(0, 0, 13, 6);
-    pop();
-}
-
-
-//生成パターン
-function genCommand(repeat) {
-    let command = "X";
-    for (let i = 0; i < repeat; i++) {
-        let newCommand = "";
-        for (let j = 0; j < command.length; j++) {
-            switch (command[j]) {
-                case "F":
-                    if (Math.random() < 0.5) {
-                        newCommand += "FF";
-                    } else {
-                        newCommand += "F-F";
-                    }
-                    break;
-                case "X":
-                    newCommand += "F[+X]F[-X+]+X";
-                    break;
-                default:
-                    newCommand += command[j];
-                    break;
-            }
-        }
-        command = newCommand;
-    }
-    return command;
-}
-
+	//生成パターン
+	function genCommand(repeat) {
+		let command = "X";
+		for (let i = 0; i < repeat; i++) {
+			let newCommand = "";
+			for (let j = 0; j < command.length; j++) {
+				switch (command[j]) {
+					case "F":
+						if (Math.random() < 0.5) {
+							newCommand += "FF";
+						} else {
+							newCommand += "F-F";
+						}
+						break;
+					case "X":
+						newCommand += "F[+X]F[-X+]+X";
+						break;
+					default:
+						newCommand += command[j];
+						break;
+				}
+			}
+			command = newCommand;
+		}
+		return command;
+	}
 }
